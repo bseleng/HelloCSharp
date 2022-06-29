16c5392fcd06f0d31199381c4f9791271be39cb5
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-06-29 19:45:13 +0300

    init: start hello world console app

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("- Hello, World!");
+Console.Write("- Hey! How ");
+Console.Write("are ");
+Console.Write("you?\n");
+Console.Write("- Long time no see!\n");
