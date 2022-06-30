442c7fc4425abb85b53c196c73092921c5899ac4
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-06-30 21:21:40 +0300

    MS-LEARN | feat: explore C# math with fractional numbers

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("- Hello, World!");
-Console.Write("- Hey! How ");
-Console.Write("are ");
-Console.Write("you?\n");
-Console.Write("- Long time no see!\n");
+Console.WriteLine(123.1m + 0.1m);
+Console.WriteLine(123.1 + 0.1);
