bb070b63cb3ff0ae2ab39672b4b80b15f87e1345
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-07-18 20:32:09 +0300

    MS-LEARN | feat: explore if else statements

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,5 +1,29 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+﻿Random dice = new Random();
 
-Console.WriteLine(largerValue);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+{
+    total += 2;
+}
+else if (roll1 == roll2 && roll1 == roll3)
+{
+    total += 6;
+}
+
+Console.WriteLine($"Updated total: {total}");
+if (total > 14)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
