d47045b172dd6cb46eaed80365d9af6b97093031
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-07-21 19:22:17 +0300

    MS-LEARN | feat: expove if else and local methods

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,35 +1,33 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+//  int daysUntilExpiration = 1;
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-
-if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+string GetDiscountMessage()
 {
-    if (roll1 == roll2 && roll1 == roll3)
+    return $"Renew now and save  {discountPercentage}%!";
+}
+
+// Your code goes here
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration < 1)
     {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
+        Console.Write("Your subscription has expired.");
+    }
+    else if (daysUntilExpiration == 1)
+    {
+        discountPercentage = 20;
+        Console.Write($"Your subscription expires within a day!\n{GetDiscountMessage()}");
+    }
+    else if (daysUntilExpiration <= 5)
+    {
+        discountPercentage = 10;
+        Console.Write($"Your subscription expires in {daysUntilExpiration} days!\n{GetDiscountMessage()}");
     }
     else
     {
-        Console.WriteLine("You rolled doubles! +2 bonus to total!");
-        total += 2;
+        Console.Write("Your subscription will expire soon. Renew now!");
+
     }
-}
-
-Console.WriteLine($"Updated total: {total}");
-if (total <= 15)
-{
-    Console.WriteLine("You win!");
-}
-else
-{
-    Console.WriteLine("Sorry, you lose.");
-
 }
