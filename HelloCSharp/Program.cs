e15583bcbb575e832d38fcbe61c0f9a577a2009d
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-07-25 20:38:30 +0300

    MS-LEARN | feat: explore foreach

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,33 +1,18 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-//  int daysUntilExpiration = 1;
-int discountPercentage = 0;
+﻿string[] fakeOrderIds = {
+"B123",
+"C234",
+"A345",
+"C15",
+"B177",
+"G3003",
+"C235",
+"B179",
+};
 
-string GetDiscountMessage()
+foreach (string fakeOrderId in fakeOrderIds)
 {
-    return $"Renew now and save  {discountPercentage}%!";
-}
-
-// Your code goes here
-if (daysUntilExpiration <= 10)
-{
-    if (daysUntilExpiration < 1)
+    if (fakeOrderId.StartsWith("B"))
     {
-        Console.Write("Your subscription has expired.");
-    }
-    else if (daysUntilExpiration == 1)
-    {
-        discountPercentage = 20;
-        Console.Write($"Your subscription expires within a day!\n{GetDiscountMessage()}");
-    }
-    else if (daysUntilExpiration <= 5)
-    {
-        discountPercentage = 10;
-        Console.Write($"Your subscription expires in {daysUntilExpiration} days!\n{GetDiscountMessage()}");
-    }
-    else
-    {
-        Console.Write("Your subscription will expire soon. Renew now!");
-
+        Console.WriteLine(fakeOrderId);
     }
 }
